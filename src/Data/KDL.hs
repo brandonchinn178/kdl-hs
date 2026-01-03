5ac6c7b122b214e695f04bba343696f4f8988787
Brandon Chinn <brandonchinn178@gmail.com>
2026-01-03 09:07:22 -0800

    Add Data.KDL.Decoder

--- src/Data/KDL.hs
+++ src/Data/KDL.hs
@@ -64,7 +64,7 @@ module Data.KDL (
   module X,
 ) where
 
-import Data.KDL.Decoder.Monad as X
+import Data.KDL.Decoder as X
 import Data.KDL.Parser as X
 import Data.KDL.Render as X
 import Data.KDL.Types as X
---
+++ src/Data/KDL/Decoder.hs
@@ -0,0 +1,10 @@
+module Data.KDL.Decoder (
+  -- * Decoder monad
+  module Data.KDL.Decoder.Monad,
+
+  -- * Schema
+  module Data.KDL.Decoder.Schema,
+) where
+
+import Data.KDL.Decoder.Monad
+import Data.KDL.Decoder.Schema
